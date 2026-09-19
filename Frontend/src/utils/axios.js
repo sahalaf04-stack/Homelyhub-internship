e207a3ec7027c8dae482872e5a16ca86d012382d
sahala <sahalaf04@gmail.com>
2026-09-19 18:57:48 +0530

    Set production backend URL

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import qs from 'qs';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL ||'/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://homelyhub-backend.onrender.com',
     withCredentials: true,
     paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 })
